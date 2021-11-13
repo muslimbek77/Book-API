7921fe01bc68b31d6db72be3cf4942619543ce45
Muslimjonbekali <muslimjonbekali@mail.ru>
2021-11-13 17:43:57 +0500

    first-commit

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+# api/views.py
+from rest_framework import generics
+from books.models import Book
+from .serializers import BookSerializer
+
+class BookAPIView(generics.ListAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
